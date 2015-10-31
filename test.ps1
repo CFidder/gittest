c67b89f60725d8dfd20e42792f68f5ce14f719dc
Cfi <casper.fidder@solvinity.com>
2015-10-31 23:15:04 +0100

    First commit

---
+++ test.ps1
@@ -0,0 +1,13 @@
+﻿Param($input1, $input2)
+Write-Host
+Write-Host "======================"
+Write-Host "Test scripts"
+Write-Host "======================"
+Write-Host
+Write-Host "Variables"
+Write-Host "`tinput1: $input1"
+Write-Host "`tinput2: $input2"
+Write-Host
+
+
+
